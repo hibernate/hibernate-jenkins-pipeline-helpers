9cadd939e7bfbc0cb74948916d6d56048c219c89
Yoann Rodière <yoann@hibernate.org>
2023-11-08 09:50:39 +0100

    Support for version schemes without .Final qualifier

--- test/VersionTest.groovy
+++ test/VersionTest.groovy
@@ -7,14 +7,33 @@
 
 import org.hibernate.jenkins.pipeline.helpers.version.Version
 import org.junit.Assert
+import org.junit.Assume
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
+@RunWith(Parameterized.class)
 class VersionTest {
+
+	@Parameterized.Parameters(name = "{0}")
+	static Version.Scheme[] params() {
+		return Version.Scheme.values() + [null]
+	}
+
+	@Parameterized.Parameter
+	public Version.Scheme scheme
 
 	@Test
 	void release_valid() {
-		testReleaseVersion('5.10.5.Final', '5', '10', '5', 'Final')
-		testReleaseVersion('6.0.0.Final', '6', '0', '0', 'Final')
+		if (!scheme || scheme == Version.Scheme.JBOSS_CLASSIC) {
+			testReleaseVersion('5.10.5.Final', '5', '10', '5', 'Final')
+			testReleaseVersion('6.0.0.Final', '6', '0', '0', 'Final')
+		}
+		else {
+			assert scheme == Version.Scheme.JBOSS_NO_FINAL
+			testReleaseVersion('5.10.5', '5', '10', '5', null)
+			testReleaseVersion('6.0.0', '6', '0', '0', null)
+		}
 		testReleaseVersion('6.0.0.Alpha1', '6', '0', '0', 'Alpha1')
 		testReleaseVersion('6.0.0.Alpha12', '6', '0', '0', 'Alpha12')
 		testReleaseVersion('6.0.0.Beta1', '6', '0', '0', 'Beta1')
@@ -31,61 +50,95 @@ class VersionTest {
 
 	@Test(expected = IllegalArgumentException)
 	void release_snapshot() {
-		Version.parseReleaseVersion('7.15.5-SNAPSHOT')
+		doParseReleaseVersion('7.15.5-SNAPSHOT')
+	}
+
+	@Test(expected = IllegalArgumentException)
+	void release_missingFinalQualifier() {
+		Assume.assumeTrue("This test is only relevant for the JBOSS_CLASSIC version scheme",
+				scheme == null || scheme == Version.Scheme.JBOSS_CLASSIC)
+		doParseReleaseVersion('7.15.7')
+	}
+
+	@Test(expected = IllegalArgumentException)
+	void release_extraFinalQualifier() {
+		Assume.assumeTrue("This test is only relevant for the NO_FINAL version scheme",
+				scheme == Version.Scheme.JBOSS_NO_FINAL)
+		doParseReleaseVersion('7.15.7.Final')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_missingMicro() {
-		Version.parseReleaseVersion('7.15.Final')
+		if (!scheme || scheme == Version.Scheme.JBOSS_CLASSIC) {
+			doParseReleaseVersion('7.15.Final')
+		}
+		else {
+			assert scheme == Version.Scheme.JBOSS_NO_FINAL
+			doParseReleaseVersion('7.15')
+		}
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_missingMinorAndMicro() {
-		Version.parseReleaseVersion('7.Final')
+		if (!scheme || scheme == Version.Scheme.JBOSS_CLASSIC) {
+			doParseReleaseVersion('7.Final')
+		}
+		else {
+			assert scheme == Version.Scheme.JBOSS_NO_FINAL
+			doParseReleaseVersion('7')
+		}
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_unknownQualifier() {
-		Version.parseReleaseVersion('7.15.5.Foo')
+		doParseReleaseVersion('7.15.5.Foo')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_finalQualifierWithNumber() {
-		Version.parseReleaseVersion('7.15.5.Final2')
+		doParseReleaseVersion('7.15.5.Final2')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_nonZeroMicroForAlpha() {
-		Version.parseReleaseVersion('7.15.5.Alpha1')
+		doParseReleaseVersion('7.15.5.Alpha1')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_nonZeroMicroForBeta() {
-		Version.parseReleaseVersion('7.15.5.Beta2')
+		doParseReleaseVersion('7.15.5.Beta2')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void release_nonZeroMicroForCR() {
-		Version.parseReleaseVersion('7.15.5.CR3')
+		doParseReleaseVersion('7.15.5.CR3')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void development_nosnapshot() {
-		Version.parseDevelopmentVersion('7.15.5')
+		doParseDevelopmentVersion('7.15.5')
 	}
 
 	@Test(expected = IllegalArgumentException)
 	void development_extraQualifier() {
-		Version.parseDevelopmentVersion('7.15.5.Final-SNAPSHOT')
+		doParseDevelopmentVersion('7.15.5.Final-SNAPSHOT')
+	}
+
+	private Version doParseReleaseVersion(String versionString) {
+		scheme ? Version.parseReleaseVersion(versionString, scheme) : Version.parseReleaseVersion(versionString)
+	}
+
+	private Version doParseDevelopmentVersion(String versionString) {
+		scheme ? Version.parseDevelopmentVersion(versionString, scheme) : Version.parseDevelopmentVersion(versionString)
 	}
 
 	private void testReleaseVersion(String versionString, String major, String minor, String micro, String qualifier) {
-		Version version = Version.parseReleaseVersion(versionString)
+		Version version = doParseReleaseVersion(versionString)
 		assertVersion(version, versionString, major, minor, micro, qualifier, false)
 	}
 
 	private void testDevelopmentVersion(String versionString, String major, String minor, String micro, String qualifier) {
-		Version version = Version.parseDevelopmentVersion(versionString)
+		Version version = doParseDevelopmentVersion(versionString)
 		assertVersion(version, versionString, major, minor, micro, qualifier, true)
 	}
 
