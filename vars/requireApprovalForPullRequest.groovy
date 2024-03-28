2fdb5d11a99442194582553f145ab8656576eb25
Yoann Rodière <yoann@hibernate.org>
2024-03-28 15:35:37 +0100

    requireApprovalForPullRequest: Typos, formatting.

--- vars/requireApprovalForPullRequest.groovy
+++ vars/requireApprovalForPullRequest.groovy
@@ -3,20 +3,21 @@ import hudson.model.User
 
 def call(String approvalGroup) {
 	if (!env.CHANGE_AUTHOR) {
-		echo "This doesn't look like a pull request; no approval required."
+		echo "Not a pull request build; no approval required."
 		return
 	}
 	if (isMember(env.CHANGE_AUTHOR, approvalGroup)) {
-		echo "Pull request submitted by ${env.CHANGE_AUTHOR}, who is a member of ${approvalGroup}; no approval required."
+		echo "Pull request submitted by '${env.CHANGE_AUTHOR}', who is a member of '${approvalGroup}'; no approval required."
 		return
 	}
 	String buildRequesterId = currentBuild.rawBuild?.getCause(Cause.UserIdCause.class)?.getUserId()
 	if (buildRequesterId && isMember(buildRequesterId, approvalGroup)) {
-		echo "Build requested by ${buildRequesterId}, who is a member of ${approvalGroup}; no approval required."
+		echo "Build requested by '${buildRequesterId}', who is a member of '${approvalGroup}'; no approval required."
 		return
 	}
-	input message: """Approval is required to build pull request ${env.CHANGE_ID} submitted by ${env.CHANGE_AUTHOR}, who is not a member of ${approvalGroup} (or never logged in on this Jenkins instance).
-Please check the code seem safe to build: no attempt to abuse resources, inspect secrets, etc.""", submitter: approvalGroup
+	input message: """Approval is required to build pull request ${env.CHANGE_ID} submitted by '${env.CHANGE_AUTHOR}', who is not a member of '${approvalGroup}' (or never logged in on this Jenkins instance).
+
+Please check the code seems safe to build: no attempt to abuse resources, inspect secrets, etc.""", submitter: approvalGroup
 }
 
 static boolean isMember(String userId, String groupId) {
