d634f7096db36457c274df6ac3dd4f310f6724b7
Yoann Rodière <yoann@hibernate.org>
2024-05-06 13:21:16 +0200

    requireApprovalForPullRequest: Clearer and less verbose logs

--- vars/requireApprovalForPullRequest.groovy
+++ vars/requireApprovalForPullRequest.groovy
@@ -1,53 +1,75 @@
 import hudson.model.Cause
 import hudson.model.User
 
+import java.util.stream.Collectors
+
 def call(String approvalGroup) {
-	if (!needsApproval(approvalGroup)) {
-		echo "No approval required."
+	StringBuilder log = new StringBuilder()
+	boolean requireApproval = needsApproval(log, approvalGroup)
+
+	def reason = log.toString().lines()
+			.map({ it -> '    ' + it })
+			.collect(Collectors.joining('\n'))
+	if (!requireApproval) {
+		echo """No approval required.
+Reason:
+${reason}
+"""
 		return
 	}
-	input message: """Approval is required to build pull request ${env.CHANGE_ID}.
+
+		input message: """Approval is required to build pull request ${env.CHANGE_ID}.
+Reason:
+${reason}
 
 Please check the code seems safe to build: no attempt to abuse resources, inspect secrets, etc.""", submitter: approvalGroup
 }
 
-boolean needsApproval(String approvalGroup) {
+boolean needsApproval(StringBuilder log, String approvalGroup) {
 	String prAuthorId = env.CHANGE_AUTHOR
 
 	if (!prAuthorId) {
-		echo "Not a pull request build."
+		log.append("Not a pull request build.\n")
 		return false
 	}
 
-	echo "Pull request submitted by ${prAuthorId}"
-	if (isMember(prAuthorId, approvalGroup)) {
+	log.append("Pull request submitted by '${prAuthorId}'.\n")
+	if (isMember(log, prAuthorId, approvalGroup)) {
 		return false
 	}
 
 	String buildRequesterId = currentBuild.rawBuild?.getCause(Cause.UserIdCause.class)?.getUserId()
 	if (buildRequesterId) {
-		echo "Build requested by ${buildRequesterId}"
-		if (isMember(buildRequesterId, approvalGroup)) {
+		log.append("Build requested by '${buildRequesterId}'.\n")
+		if (isMember(log, buildRequesterId, approvalGroup)) {
 			return false
 		}
+	}
+	else {
+		log.append("Build not requested by a user.\n")
 	}
 
 	return true
 }
 
 
-boolean isMember(String userId, String groupId) {
+boolean isMember(StringBuilder log, String userId, String groupId) {
 	def user = User.getById(userId, false)
 	if (!user) {
-		echo "Jenkins user '${userId}' does not exist (maybe they never logged in on this Jenkins instance?)"
+		log.append("Jenkins user '${userId}' does not exist, or has never logged in on this Jenkins instance for a long time.\n")
 		return false
 	}
-	if (user.getAuthorities()?.contains(groupId)) {
-		echo "Jenkins user '${userId}' is a member of '${groupId}'."
+	def auths = user.getAuthorities()
+	if (auths?.contains(groupId)) {
+		log.append("Jenkins user '${userId}' is a member of '${groupId}'.\n")
 		return true
 	}
+	else if (auths) {
+		log.append("Jenkins user '${userId}' is not a member of '${groupId}'.\n")
+		return false
+	}
 	else {
-		echo "Jenkins user '${userId}' is not a member of '${groupId}'"
+		log.append("Jenkins user '${userId}' is not a member of '${groupId}', or hasn't logged in on this Jenkins instance for a long time.\n")
 		return false
 	}
 }
